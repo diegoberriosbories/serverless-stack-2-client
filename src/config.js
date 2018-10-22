0961b71bfc40feef8c7370ae79d05b825da6cdeb
diegoberriosbories <d.was.berrios@gmail.com>
2018-10-22 17:50:51 -0300

    Connecting the billing form

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const dev = {
-  STRIPE_KEY: "sk_test_ukGTNEnl3lmLAB6RkPBuT1fA",
+  STRIPE_KEY: "pk_test_2QAcffdhPyvgL6zKCQeumEmS",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-dev-attachmentsbucket-16tsiqu40ur1k"
@@ -12,12 +12,12 @@ const dev = {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_7hLjLceh4",
     APP_CLIENT_ID: "1tbvm30f7ln0j1n3k4vovmd84j",
-    IDENTITY_POOL_ID: "us-east-1:4c4dc813-ebda-4cfb-b779-981db18073ec"
+    IDENTITY_POOL_ID: "us-east-1:9cc1367f-055f-4b5b-8174-c939e28a7cda"
   }
 };
 
 const prod = {
-  STRIPE_KEY: "sk_test_ukGTNEnl3lmLAB6RkPBuT1fA",
+  STRIPE_KEY: "pk_test_2QAcffdhPyvgL6zKCQeumEmS",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-1dft0qj6dgrxo"
